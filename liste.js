25ad61a2dd44e883ff12b8e6e6f55fe764637277
renzo <you@example.com>
2017-01-13 12:13:08 +0100

    liste a l'air de marcher

--- liste.js
+++ liste.js
@@ -1,12 +1,32 @@
-var listeApp = angular.module('listeApp', []);
+var listeApp = angular.module('listeApp', ['ngSanitize']);
 var recettes = [
     {
         name:'Poulet champi crème',
         ingredients: [
         {
+            'name': 'Riz',
+            'qty': 100,
+            'unit':"g"
+        },
+        {
             'name': 'Filet de poulet',
             'qty': 1,
             'unit':""
+        },
+        {
+            'name': 'Vin blanc de cuisine',
+            'qty': 75/10,
+            'unit':"cL"
+        },
+        {
+            'name': 'Oignons',
+            'qty': 50,
+            'unit':"g"
+        },
+        {
+            'name': 'Crème fraiche',
+            'qty': 100/14,
+            'unit':"cL"
         },
         {
             'name': 'Champignons',
@@ -35,27 +55,8 @@ var recettes = [
             },
         ]
     },
-    ];
+];
 
-
-function getIngredients(r) {
-    for (a in recettes) {
-        recette = recettes[a];
-        if (recette.name == r) {
-            return recette.ingredients;
-        }
-    }
-}
-
-function addToListe(liste, i, nb) {
-    if (i.name in liste) {
-        var cur_qty =  liste[i.name];
-        liste[i.name] = cur_qty + i.qty * nb;
-    } else {
-        liste[i.name] = i.qty * nb; 
-    }
-    return liste;
-}
 range = function(max) {
     var res = [];
     for (var i=1; i<=max; i++) {res.push(i)};
@@ -64,34 +65,44 @@ range = function(max) {
 
 listeApp.controller('ListeCtrl', function ListeCtrl($scope) {
     $scope.recettes = recettes;
-    $scope.liste_text="Liste ";
     $scope.nb_jours = "5"; // srsly!
     $scope.nb_gens = "10"; // srsly!
     $scope.jours_tab = range(20); 
     $scope.gens_tab = range(25); 
 
     $scope.gens_par_jour = new Array( parseInt($scope.nb_jours)).fill($scope.nb_gens);
-    console.log($scope.gens_par_jour);
     $scope.recette_par_jour = new Array( parseInt($scope.nb_jours)).fill("");
+
+    var liste_json=[];
 
 
     $scope.updateListe = function() {
-        var liste_json={};
-        var all_ingredients={};
-        for (i = 0; i < $scope.nb_jours; i++) {
+        liste_json=[];
+        for (var i = 0; i < $scope.nb_jours; i++) {
             r = $scope.recette_par_jour[i];
-            g = $scope.gens_par_jour[i];
+            g = parseInt($scope.gens_par_jour[i]);
             if (r == "") {
                 continue;
             }
-            var ings = getIngredients(r)
+            var ings = getIngredients(r);
             for (ing in ings) {
                 ingredient = ings[ing]; 
                 addToListe(liste_json, ingredient, g);
-            };
+            }
         };
-        console.log(liste_json);
+        changeListeText(liste_json);
     };
+
+    changeListeText = function(j){
+        h = "<ul>\n";
+        for (var i in j) {
+            var item = j[i];
+            console.log(item);
+            h += "<ul>"+item.name+": "+arrondi(item.qty, item.unit)+"</ul>\n" ;
+        }
+        h+="</ul>";
+        $scope.liste_html=h;
+    }
 
     $scope.joursChanged = function() {
         $scope.jours = new Array(+$scope.nb_jours);
@@ -103,6 +114,58 @@ listeApp.controller('ListeCtrl', function ListeCtrl($scope) {
 
     }
     $scope.joursChanged();
+
+
+    getIngredients = function(r) {
+        for (a in recettes) {
+            recette = recettes[a];
+            if (recette.name == r) {
+                return recette.ingredients;
+            }
+        }
+    }
+
+    arrondi = function(nb, unit) {
+        console.log(nb+" "+unit);
+        var n = 0;
+        switch(unit) {
+            case "g":
+                if (nb>=1000) {
+                    n = Math.round(nb / 100) / 10;
+                    return n+"kg";
+                }
+                break;
+            case "cL":
+                if (nb>=100) {
+                    n = Math.round(nb / 10) / 10;
+                    return n+"L";
+                }
+                break;
+        }
+        return Math.round(nb)+""+unit;
+    }
+
+    addToListe = function(liste, ing, nb) {
+        console.log("pour "+nb);
+        console.log(ing);
+        var found = false;
+        for (i in liste) {
+            var item = liste[i];
+            if (item.name == ing.name) {
+                item.qty = item.qty + (ing.qty * nb);
+                found = true;
+            }
+        }
+        if (!found) {
+            //need to dupe, wtf
+            var item = {};
+            item.name = ing.name;
+            item.qty = ing.qty * nb;
+            item.unit = ing.unit;
+            liste.push(item);
+        }
+        console.log(liste);
+    }
 
 });
 
