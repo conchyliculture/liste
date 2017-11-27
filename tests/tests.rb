c4d666d31ad609260b83282bbe21958824e0f885
renzo <github@renzokuken.eu>
2017-11-27 13:05:44 +0100

    user power assert

--- tests/tests.rb
+++ tests/tests.rb
@@ -32,15 +32,9 @@ class TestListe < Test::Unit::TestCase
         get '/recettes.json'
         assert last_response.ok?
         j = JSON.parse(last_response.body)
-        assert_block do
-            j['recettes'].size > 5
-        end
-        assert_block do
-            j['recettes'][0]["name"] =~/^[a-z ]+$/i
-        end
-        assert_block do
-            j['recettes'][0]["ingredients"].size > 3
-        end
+        assert { j['recettes'].size > 5}
+        assert_block {j['recettes'][0]["name"] =~/^[a-z ]+$/i}
+        assert_block { j['recettes'][0]["ingredients"].size > 3}
     end
 
     def test_it_gives_index
@@ -56,13 +50,12 @@ class TestListe < Test::Unit::TestCase
         end
         get '/get-stored-listes'
         assert last_response.ok?
-        b = last_response.body
-        assert_equal("[{\"name\":\"lolilol\",\"date\":\"2017-01-25T16-26-38\"}]",b)
+        assert {last_response.body, "[{\"name\":\"lolilol\",\"date\":\"2017-01-25T16-26-38\"}]"}
     end
 
     def test_it_gives_stored_list
         get '/get-stored-liste', params={}
-        assert_equal(false, last_response.ok?)
+        assert {!last_response.ok?}
 
         File.open(File.join($recettes_dir,"#{$test_liste[:date]}-#{$test_liste[:name]}.json"),'w+') do |f|
             f.write($test_liste.to_json)
@@ -71,21 +64,17 @@ class TestListe < Test::Unit::TestCase
         get '/get-stored-liste', params={'name'=> $test_liste[:name]+" - "+$test_liste[:date]}
         assert last_response.ok?
         b = last_response.body
-        assert_equal($test_liste.to_json,b)
+        assert {b == $test_liste.to_json}
     end
 
     def test_it_saves_list
-        post '/save', $test_liste.to_json 
+        post '/save', $test_liste.to_json
         assert last_response.ok?
-        b = last_response.body
-        assert_equal("done", b)
+        assert { last_response.body == "done"}
 
         recette = File.read(Dir.glob($recettes_dir+"/*.json")[0])
         j = JSON.parse(recette)
-        assert_equal("lolilol", j["name"])
-        assert_block do 
-            j["date"] == Time.now.strftime('%Y-%m-%dT%H-%M-%S')
-        end
+        assert {j["name"] == "lolilol"}
+        assert {j["date"] == Time.now.strftime('%Y-%m-%dT%H-%M-%S')}
     end
-
 end
